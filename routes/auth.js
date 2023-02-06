4140f4e8ab3021a769112f5e69af41fc6bcc99b0
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2023-02-06 10:03:55 +0200

    updated api

--- routes/auth.js
+++ routes/auth.js
@@ -5,46 +5,51 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 //REGISTER
+
 router.post("/register", async (req, res) => {
  
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedpassword = await bcrypt.hash(req.body.password,salt);
-  User.find({email:req.body.email})
-      .exec()
-      .then(user=>{
-          if(user.length >= 1){
-              return res.status(409).json({
-                  message:'user already exists'
-              });
-          } else{
-                // Create a user
-  const user = new User({
-      username: req.body.username,  
-      email: req.body.email, 
-      password: hashedpassword,
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.password,salt);
   
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+        if(user.length >= 1){
+        return res.status(409).json({
+         message: 'Email already exists'
+        });
+        } else {
+                // Create a user
+         const newUser = new User({
+        username: req.body.username,  
+         email: req.body.email, 
+        password: hashedpassword
+        });
+                
+        newUser.save()
+        .then(result => {
+        const token = jwt.sign({id: result._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+         res.status(201).json({
+        status: 'success',
+        token,
+         data: {
+        user: result
+        }
+        });
+        })
+        .catch(error => {
+        res.status(500).json({
+        error
+        });
+        });
+        }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error
+            });
+        });
   });
-  const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRES_IN});
-  res.status(201).json({
-      status: 'success',
-      token,
-      data:{
-          User: user
-      }
-  });
-  // Save user in the database
-  user.save()
-  .then(data => {
-     return res.send(data);
-  }).catch (err => {
-      return res.status(500).json(err);
-  })
-          }
-
-
-})
-});
 
 //LOGIN
 router.post("/login", async (req, res) => {
